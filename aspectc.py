5e4b36edfbe80a35f37364c021465064b6dc48f4
Rigner <frasse_l@epitech.eu>
2016-09-24 17:45:14 +0200

    Added @end

--- aspectc.py
+++ aspectc.py
@@ -1,3 +1,4 @@
+from cnorm.nodes import BlockStmt, If, While, For, Return
 from pyrser import meta
 from pyrser.grammar import Grammar
 from cnorm.parsing.declaration import Declaration
@@ -39,6 +40,29 @@ def add_asp(self, *args):
     return True
 
 
+def recur_body(rec, v):
+    ok = False
+    i = 0
+    while i < len(rec.body):
+        if isinstance(rec.body[i], If) and isinstance(rec.body[i].thencond, BlockStmt):
+            recur_body(rec.body[i].thencond, v)
+        if isinstance(rec.body[i], If) and isinstance(rec.body[i].elsecond, BlockStmt):
+            recur_body(rec.body[i].elsecond, v)
+        if isinstance(rec.body[i], While) and isinstance(rec.body[i].body, BlockStmt):
+            recur_body(rec.body[i].body, v)
+        if isinstance(rec.body[i], For) and isinstance(rec.body[i].body, BlockStmt):
+            recur_body(rec.body[i].body, v)
+        if isinstance(rec.body[i], Return):
+            if i == 0:
+                rec.body = [v] + rec.body
+            else:
+                rec.body = rec.body[:len(rec) - 1] + [v] + rec.body[i:]
+            i += 1
+            ok = True
+        i += 1
+    return ok
+
+
 aspectC = AspectC()
 if len(sys.argv) > 1:
     res = aspectC.parse_file(sys.argv[1])
@@ -52,5 +76,10 @@ for t in b:
         if hasattr(r, '_name') and r._name == t[1]:
             if t[0] == "@begin":
                 r.body.body = [t[2]] + r.body.body
+            elif t[0] == "@end":
+                r = r.body
+                if not recur_body(r, t[2]):
+                    r.body = r.body + [t[2]]
+
 
 print(res.to_c())
