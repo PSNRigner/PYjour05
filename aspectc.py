007819c5ea52ac2c1aee2de79a16694bb386fb68
Rigner <frasse_l@epitech.eu>
2016-09-25 11:26:57 +0200

    Added @callback

--- aspectc.py
+++ aspectc.py
@@ -1,4 +1,6 @@
-from cnorm.nodes import BlockStmt, If, While, For, Return
+from _weakref import ref
+
+from cnorm.nodes import *
 from pyrser import meta
 from pyrser.grammar import Grammar
 from cnorm.parsing.declaration import Declaration
@@ -20,6 +22,13 @@ class AspectC(Grammar, Declaration):
         #add_asp(tag, name, statement)
     ]
 
+    callback_decl = [
+        "@callback"
+        '(' id:name ')'
+
+        #add_asp(name)
+    ]
+
     declaration = [
             ';' // garbage single comma
             |
@@ -30,13 +39,35 @@ class AspectC(Grammar, Declaration):
             asm_decl
             |
             asp_decl
+            |
+            callback_decl
         ]
     """
 
 
+# noinspection PyProtectedMember
 @meta.hook(AspectC)
 def add_asp(self, *args):
-    b.append((self.value(args[0]), self.value(args[1]), args[2]))
+    if len(args) == 1:
+        name = self.value(args[0])
+        for m in self.rule_nodes.maps:
+            body = m.get('_', None)
+            if body is not None and isinstance(body, RootBlockStmt):
+                root = body
+                body = body.body
+                for bod in body:
+                    if hasattr(bod, '_name') and bod._name == name:
+                        ctype = PrimaryType(bod.ctype._identifier)
+                        ctype._storage = 2
+                        ctype._decltype = PointerType()
+                        ctype._decltype._decltype = ParenType(bod.ctype._params)
+                        # for par in ctype._decltype._decltype:
+                        decl = Decl("callback_" + name, ctype)
+                        body.append(decl)
+                        root.types[decl._name] = ref(decl)
+
+    else:
+        b.append((self.value(args[0]), self.value(args[1]), args[2]))
     return True
 
 
@@ -51,6 +82,10 @@ def recur_body(rec, v):
         if isinstance(rec.body[i], While) and isinstance(rec.body[i].body, BlockStmt):
             recur_body(rec.body[i].body, v)
         if isinstance(rec.body[i], For) and isinstance(rec.body[i].body, BlockStmt):
+            recur_body(rec.body[i].body, v)
+        if isinstance(rec.body[i], Do) and isinstance(rec.body[i].body, BlockStmt):
+            recur_body(rec.body[i].body, v)
+        if isinstance(rec.body[i], Switch) and isinstance(rec.body[i].body, BlockStmt):
             recur_body(rec.body[i].body, v)
         if isinstance(rec.body[i], Return):
             if i == 0:
@@ -67,10 +102,9 @@ aspectC = AspectC()
 if len(sys.argv) > 1:
     res = aspectC.parse_file(sys.argv[1])
 else:
-    res = aspectC.parse_file("test.c")
+    res = aspectC.parse_file("test2.c")
 
 for t in b:
-    # print(t[0], t[1], t[2].body)
     for r in res.body:
         # noinspection PyProtectedMember
         if hasattr(r, '_name') and r._name == t[1] and hasattr(r, 'body') and hasattr(r.body, 'body'):
